25517319c5ffcecd0df1383c159e803def6adc65
Talha Shahid <talhashahidch@gmail.com>
2022-02-22 17:20:47 +0500

    User can add Note to LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log("Welcome");
+
+//if user adds a note, add it to the LocalStorage
+
+let addBtn=document.getElementById('addBtn');
+
+addBtn.addEventListener("click", function(e){
+    let addTxt = document.getElementById('addTxt');
+    let notes =localStorage.getItem('notes');
+
+    if(notes==null){
+        notesObj=[];
+    }
+    else{
+        notesObj=JSON.parse(notes)
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    addTxt.value=" ";
+})
